80b5d72050d4a6b08b510a6aba4322cf71702fac
acj <gitlab@acjs.net>
2019-03-11 12:50:52 +0100

    Allow plugin to be installed.

---
+++ deplug/deplug.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name: dePlug
+ * Description: Add Google Analytics, Open Graph, Twitter Cards, et al.
+ * Version: 0.1
+ * Author: deidee
+ * Author URI: https://deidee.nl/
+ * License: GPL3
+ * License URI: https://www.gnu.org/licenses/gpl-3.0.html
+ * Text Domain: deplug
+ */
+
+defined( 'ABSPATH' ) or die( 'Computer says no.' );
+
---
+++ deplug/readme.txt
@@ -0,0 +1,10 @@
+=== Plugin Name ===
+Contributors: acjbizar
+Donate link: https://deidee.com/
+Tags: seo, opengraph, twittercard
+Requires at least: 4.6
+Tested up to: 4.7
+Stable tag: 4.3
+Requires PHP: 5.2.4
+License: GPLv3
+License URI: https://www.gnu.org/licenses/gpl-3.0.html
